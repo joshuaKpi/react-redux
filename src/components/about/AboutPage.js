58b8bef76527ff39c0c1b5f9c18730b99e65810c
joshuaKpi <joshua.kpi@gmail.com>
2017-07-12 14:12:26 +0300

    Added react-router and two pages routing using browserHistory

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Sone particular paragraph about this site created to train skills</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
