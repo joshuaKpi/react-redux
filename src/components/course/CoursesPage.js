9b37182fddfab9f01d98e5e6b7e2f235020110e5
joshuaKpi <joshua.kpi@gmail.com>
2017-07-13 12:40:00 +0300

    Another way to dispatch action

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -27,7 +27,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   }
 
   render() {
@@ -53,8 +53,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -63,7 +63,13 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
 
 // const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
 //export default connectedStateAndProps(CoursePage);
