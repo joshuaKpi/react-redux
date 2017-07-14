f45825d38de9369eef307fadae5c96f0234e19bb
joshuaKpi <joshua.kpi@gmail.com>
2017-07-14 12:41:46 +0300

    Add initialState because now app has >1 reducer which we use in our reducers

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import routes from './routes';
 import {loadCourses} from './actions/courseActions';
+import {loadAuthors} from './actions/authorActions';
 // import CSS by Webpack
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
   <Provider store={store}>
