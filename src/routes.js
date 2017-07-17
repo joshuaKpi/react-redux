d1517dc7fa3444f03c4839b3478328a7b6e2b69b
joshuaKpi <joshua.kpi@gmail.com>
2017-07-17 13:52:31 +0300

    onEnter and onLeave hooks play around

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,22 @@ import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
 import ManageCoursePage from './components/course/ManageCoursePage';
 
+import initialState from './reducers/initialState';
+
+function onEnterCall() {
+  console.log('onEnter hook triggered');
+  initialState.counter++;
+  window.localStorage.setItem('onEnterHookCounter', initialState.counter);
+}
+
+function onLeaveCall() {
+  console.log('onLeave hook triggered');
+}
+
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
-    <Route path="about" component={AboutPage} />
+    <Route path="about" component={AboutPage} onEnter={onEnterCall} onLeave={onLeaveCall} />
     <Route path="courses" component={CoursesPage} />
     <Route path="course" component={ManageCoursePage} />
     <Route path="course/:id" component={ManageCoursePage} />
