f39d8ffd4369899f627e13eb9b6ef2b061893208
joshuaKpi <joshua.kpi@gmail.com>
2017-07-17 12:29:28 +0300

    Show message on submit error

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import toastr from 'toastr';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
@@ -34,11 +35,17 @@ class ManageCoursePage extends React.Component {
   saveCourse(event) {
     event.preventDefault();
     this.setState({loading: true});
-    this.props.actions.saveCourse(this.state.course).then(() => this.redirect());
+    this.props.actions.saveCourse(this.state.course)
+      .then(() => this.redirect())
+      .catch(error => {
+        toastr.error(error);
+        this.setState({loading: false});
+      });
   }
 
   redirect() {
     this.context.router.push('/courses');
+    toastr.success('Course saved');
     this.setState({loading: false});
   }
 
